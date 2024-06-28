e6ecbf30fbf60b5506aa892bb6ba8c297134d75a
ramadhanni <ramadhani05112003@gmail.com>
2024-06-28 23:06:50 +0700

    cek

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class _LoginScreenState extends State<LoginScreen> {
   String _passwordErrorText = '';
 
   Future<String> login(String username, String password) async {
-    String url = 'http://192.168.1.12/api_proyek3/login.php';
+    String url = 'http://192.168.137.1/api_proyek3/Config-mobile/login.php';
     try {
       final response = await http.post(Uri.parse(url), body: {
         "username": username,
